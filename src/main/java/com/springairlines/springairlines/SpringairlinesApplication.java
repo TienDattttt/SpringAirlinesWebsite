fdb4d8b84be498748d514f1b92aa18b124de6f14
Tien Dat <nguyentiendat201204@gmail.com>
2024-10-16 19:48:10 +0700

    empty project

---
+++ src/main/java/com/springairlines/springairlines/SpringairlinesApplication.java
@@ -0,0 +1,13 @@
+package com.springairlines.springairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringairlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringairlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springairlines/springairlines/SpringairlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springairlines.springairlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringairlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
